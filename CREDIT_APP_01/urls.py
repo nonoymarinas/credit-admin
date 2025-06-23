c5ef6af4452a963ac16b7f0eed98ad0024ca9732
nonoymarinas <nonoymarinas@gmail.com>
2025-06-23 23:08:37 +0800

    m

--- CREDIT_APP_01/urls.py
+++ CREDIT_APP_01/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('home', views.home, name='home'),
+    path('newcredit', views.newcredit, name='newcredit'),
 ]
--- CREDIT_APP_01/views.py
+++ CREDIT_APP_01/views.py
@@ -3,7 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     try:
-        return render(request, 'CREDIT_APP_01/home.html',{"year":"2025"})
+        return render(request, 'CREDIT_APP_01/home.html')
+    except Exception as e:
+        # Log the exception or handle it as needed
+        print(f"An error occurred: {e}")
+        return render(request, 'CREDIT_APP_01/error.html', {'error': str(e)})
+    
+def newcredit(request):
+    try:
+        return render(request, 'CREDIT_APP_01/newcredit.html')
     except Exception as e:
         # Log the exception or handle it as needed
         print(f"An error occurred: {e}")
