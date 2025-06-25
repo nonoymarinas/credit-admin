25327f61b3228386cb1d5dc726c4fae9834f303a
FELICIANO JR MARINAS <nonoymarinas@live.com>
2025-06-25 21:53:57 +0800

    m

--- CREDIT_ADMIN/settings.py
+++ CREDIT_ADMIN/settings.py
@@ -125,9 +125,6 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [
-    BASE_DIR / "static/css",
-    BASE_DIR / "static/js",
-    BASE_DIR / "static/img",
     BASE_DIR / "static",
 ]
 # Default primary key field type
